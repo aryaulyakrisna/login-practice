fcffc427698dc734107dd010be0c1a0f1b74bfe8
aryaulyakrisna117 <aryaulyakrisna@gmail.com>
2024-04-03 05:11:12 +0700

    Added: gitignore

--- src/server.js
+++ src/server.js
@@ -1,10 +1,19 @@
 const cors = require("cors");
+require('dotenv').config();
 
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
+
+// Routes
+
+
+// Middlewares
 app.use(cors());
+app.use(express.json());
+
 
 app.listen(PORT, () => {
   console.log("Listening on port " + PORT);
 });
+
